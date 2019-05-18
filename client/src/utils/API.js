f2c8d1ad34d97e3f39006e70fae5b159715fc7d5
Lynn <43070638+LynnAmsbury@users.noreply.github.com>
2019-05-18 09:40:52 -0600

    Fixed hard-coded PORT 3002 routes

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,18 +3,18 @@ import axios from "axios";
 export default {
   // Gets books from the Google API
   getBooks: function(q) {
-    return axios.get("http://localhost:3002/api/google", { params: { q: "title:" + q } });
+    return axios.get("/api/google", { params: { q: "title:" + q } });
   },
   // Gets all saved books
   getSavedBooks: function() {
-    return axios.get("http://localhost:3002/api/books");
+    return axios.get("/api/books");
   },
   // Deletes the saved book with the given id
   deleteBook: function(id) {
-    return axios.delete("http://localhost:3002/api/books/" + id);
+    return axios.delete("/api/books/" + id);
   },
   // Saves an book to the database
   saveBook: function(bookData) {
-    return axios.post("http://localhost:3002/api/books", bookData);
+    return axios.post("/api/books", bookData);
   }
 };
